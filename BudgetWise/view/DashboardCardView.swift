03d3a90a185dce31c9cc55ab0935e1ae045bd286
osmar <techsmart.dev@gmail.com>
2023-01-18 22:02:08 -0600

    custom style added for dashboard ecard

--- BudgetWise/view/DashboardCardView.swift
+++ BudgetWise/view/DashboardCardView.swift
@@ -12,33 +12,40 @@ struct DashboardCardView: View {
     var body: some View {
         VStack(alignment: .center) {
                     Text(dashCard.amountMoney)
-                .font(.title)
+                .font(.largeTitle)
+                        .foregroundColor(.white)
                     Text(dashCard.txtAvailableMoney)
-                .font(.subheadline)
+                        .foregroundColor(Color("GrayCustom"))
+                        .font(.title3)
                     HStack {
                         VStack(alignment: .center) {
-                        Button(action: addNewIncomeAction){
-                            HStack {Image(systemName: "clock")
-                            }.padding(10.0)
-                                .frame(width: 50, height:50, alignment: .center)
-                            .overlay(RoundedRectangle(cornerRadius: 50.0)
-                                .stroke(lineWidth: 1.0))}
+                            Button(action: addNewIncomeAction){
+                                HStack {Image("sym_income")}.padding(10.0)
+                                    .frame(width: 55, height:55, alignment: .center)
+                                }
+                                .accessibilityIdentifier("btn_add_income")
+                                .background(RoundedRectangle(cornerRadius: 50.0))
+                                .foregroundColor(Color("GreenCustom"))
+                                .opacity(0.65)
                             Text(dashCard.txtIncome)
-                                .font(.subheadline)
+                                .font(.title3)
+                                .foregroundColor(Color("GrayCustom"))
                         }.padding(5)
                         VStack(alignment: .center) {
-                        Button(action: addNewExpenseAction){
-                            HStack {Image(systemName: "plus")
-                            }.padding(10.0)
-                                .frame(width: 50, height:50, alignment: .center)
-                            .overlay(RoundedRectangle(cornerRadius: 50.0)
-                            .stroke(lineWidth: 1.0))}
-                            Text(dashCard.txtExpense).font(.subheadline)
+                            Button(action: addNewExpenseAction){
+                                HStack {Image("sym_expense")}
+                                    .padding(10.0)
+                                    .frame(width: 55, height:55, alignment: .center)
+                                }
+                                .accessibilityIdentifier("btn_add_expense")
+                                .background(RoundedRectangle(cornerRadius: 50.0))
+                                .foregroundColor(Color("RedCustom"))
+                                .opacity(0.65)
+                            Text(dashCard.txtExpense).font(.title3)
+                                .foregroundColor(Color("GrayCustom"))
                         }.padding(5)
                     }
-        }.fixedSize()
-            .frame(width: 350, height: 220, alignment: .center)
-        
+        }.frame(width: 350, height: 220, alignment: .center)
     }
                                
    func addNewIncomeAction() {}
@@ -50,7 +57,7 @@ struct DashboardCardView_Previews: PreviewProvider {
     static var dashCard = DashboardCard.sampleData[0]
     static var previews: some View {
         DashboardCardView(dashCard: dashCard)
-            .background(.yellow)
+            .background(Color("BlueStrong"))
             .previewLayout(.fixed(width: 400, height: 220))
     }
 }
