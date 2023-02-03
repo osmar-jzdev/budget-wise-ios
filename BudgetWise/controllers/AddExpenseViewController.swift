c3263303baa6febc42fdf8f26bbc7dff1400360c
osmar <techsmart.dev@gmail.com>
2023-02-02 23:14:03 -0600

    validations added in forms income and expense

--- BudgetWise/controllers/AddExpenseViewController.swift
+++ BudgetWise/controllers/AddExpenseViewController.swift
@@ -13,14 +13,33 @@ class AddExpenseViewController: UIViewController {
     @IBOutlet var lblExpenseValue: UITextField!
     @IBOutlet var btnAdd: UIButton!
     @IBOutlet var btnClose: UIBarButtonItem!
+    @IBOutlet var lblErrorMessage: UITextView!
+    private var expenseName: String!
+    private var expenseValue: Float!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        lblErrorMessage.isHidden = true
     }
     
     @IBAction func btnAddAction(_ sender: UIButton) {
+        if ((lblExpenseName.text?.isEmpty) != nil) && ((lblExpenseValue.text?.isEmpty) != nil) {
+            expenseName = lblExpenseName.text
+            expenseValue = (lblExpenseValue.text! as NSString).floatValue
+        }
+        
+        if !expenseName.isEmpty && !expenseValue.isZero {
+            lblErrorMessage.isHidden = true
+            let alertController = UIAlertController(title: "Agregado", message: "Gasto guardado exitosamente", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Enterado", style: .default, handler: {_ in
+                self.dismiss(animated: true)
+            })
+            alertController.addAction(action)
+            self.present(alertController, animated: true)
+            
+        } else {
+            lblErrorMessage.isHidden = false
+        }
     }
     
     @IBAction func btnCloseAction(_ sender: UIBarButtonItem) {
--- BudgetWise/controllers/AddIncomeViewController.swift
+++ BudgetWise/controllers/AddIncomeViewController.swift
@@ -8,19 +8,40 @@
 import UIKit
 
 class AddIncomeViewController: UIViewController {
+    
     @IBOutlet var lblIncomeName: UITextField!
     @IBOutlet var lblIncomeValue: UITextField!
     @IBOutlet var btnAdd: UIButton!
     @IBOutlet var btnClose: UIBarButtonItem!
+    @IBOutlet var lblErrorMessage: UITextView!
+    private var incomeName: String!
+    private var incomeValue: Float!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        lblErrorMessage.isHidden = true
     }
     
     @IBAction func btnAddAction(_ sender: UIButton) {
+        if ((lblIncomeName.text?.isEmpty) != nil) && ((lblIncomeValue.text?.isEmpty) != nil) {
+            incomeName = lblIncomeName.text
+            incomeValue = (lblIncomeValue.text! as NSString).floatValue
+        }
+        
+        if !incomeName.isEmpty && !incomeValue.isZero {
+            lblErrorMessage.isHidden = true
+            let alertController = UIAlertController(title: "Agregado", message: "Ingreso guardado exitosamente", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Enterado", style: .default, handler: {_ in
+                self.dismiss(animated: true)
+            })
+            alertController.addAction(action)
+            self.present(alertController, animated: true)
+            
+        } else {
+            lblErrorMessage.isHidden = false
+        }
     }
+    
     @IBAction func btnCloseAction(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
